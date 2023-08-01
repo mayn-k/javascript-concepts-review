c204c28c0106337a26e71cb16a1c87d0657188d0
mayn-k <mayank007mk7@gmail.com>
2023-08-01 12:47:54 +0530

    the basics: objects

--- app/js/types.js
+++ app/js/types.js
@@ -1,5 +1,14 @@
-console.log('it works!'); 
+// console.log('it works!'); 
 
-const name = 'wes';
-const middle = "topher";
-const last = `bos`;
+// const name = 'wes';
+// const middle = "topher";
+// const last = `bos`;
+
+// const age = 100;
+// const name = 'wes';
+
+const person = {
+    first: `wes`,
+    last: `bos`,
+    age: 10,
+}
